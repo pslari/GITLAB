f3e39de7f9bd4de463649486b89b84c98002f9a9
fufunana <sirada.fuseela@mail.kmutt.ac.th>
2017-04-28 18:46:10 +0700

    User B try to commit same time

--- Program #1.c
+++ Program #1.c
@@ -1,13 +1,14 @@
 
 void program_1(){
 
-    printf("Hi!");
+    printf("Hi! This is B, Yay!");
     return;
 }
 
 int main(){
 
     program_1();
+    printf("Hope to see ya, A!");
 
     return 0;
 }
---
+++ Program #2.c
@@ -0,0 +1,9 @@
+
+int main(){
+   
+    printf("Hello world!");
+    // User B change something in program #2
+   
+    return 1;
+}
+
